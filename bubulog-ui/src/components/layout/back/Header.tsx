64e6128057da1ab08b95bbb7d90dbe6157ff960d
lihainuo <lihainuo0502@gmail.com>
2025-05-15 20:49:32 +0800

    refactor:修改Header, 动态标签组件样式

--- bubulog-ui/src/components/layout/back/Header.tsx
+++ bubulog-ui/src/components/layout/back/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="bg-background w-full h-auto border-b">
       <div className="flex items-center p-2.5">
-        <SidebarTrigger className="mr-4" />
+        <SidebarTrigger className="mx-4" />
       </div>
       <div className="px-4 flex-1 w-full">
         <DraggableTabs />
